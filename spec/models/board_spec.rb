8e2b3ff799a33c149d992f4619a936b903e7baf1
Dirk Dobson <dirkvancouver@live.com>
2018-09-10 16:03:57 -0600

    tonys code

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    it 'has a name' do
+      name = 'Chores Board'
+      board = Board.create(name: name)
+      expect(board.name).to eq(name)
+    end
+  end
 end
