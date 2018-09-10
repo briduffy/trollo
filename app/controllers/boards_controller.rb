fceb22169fe2ad58d45fe8695a70364d9281848f
tchoutdara <monkeymilitia444@gmail.com>
2018-09-10 16:23:44 -0600

    save info

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,41 @@
 class BoardsController < ApplicationController
+
+  before_action :set_board, only: [:show, :edit, :update, :destroy]
+
   def index
+    @boards = Board.all
   end
 
   def show
   end
 
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.dave
+      redirect_to 
   end
 
   def edit
+
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def set_board
+      @board = Board.find(params[:id])
+    end
+
+    def board_params
+      params.require(:board).permit(:name)
+    end
 end
