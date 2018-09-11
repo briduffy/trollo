ee005956d3abae54e8b96f805201e3455d83c643
Brianne Duffy <duffymbrianne@gmail.com>
2018-09-10 20:46:57 -0600

    updated lists controller/model for priority, navbbar, materlialize

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,29 @@ class List < ApplicationRecord
   belongs_to :board
   has_many :tasks
 
-  def self.by_list
+  def self.by_priority
+    order(:priority)
   end
 
+  def list_is_first
+    self.priority === 0
+  end
+
+  def self.priority_when_added
+    @list = @board.lists.new(list_params)
+    lowest = @board.lists.last('priority')
+    @list.priority = lowest + 1
+  end 
+
+  def self.up_priority
+    list_down_priority = @board.lists.find_by(priority: @list.priority - 1)
+    @list.update(priority: list_down_priority.priority)
+    list_down_priority.update(priority: list_down_priority + 1)
+  end
+
+  def self.down_priority
+    list_up_priority = @board.lists.find_by(priority: @list.priority + 1)
+    @list.update(priority: list_up_priority.priority)
+    list_up_priority(priority: list_up_priority.priority - 1)
+  end
 end
