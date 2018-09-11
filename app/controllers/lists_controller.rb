7cf637004ca064fddd8427e84ca0a952da95481e
Dirk Dobson <dirkvancouver@live.com>
2018-09-11 13:49:29 -0600

    bris code

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,24 +1,79 @@
 class ListsController < ApplicationController
-  before_action :set_list
-  before_action :set_list, only: [:show]
+  before_action :set_board
+  before_action :set_list, only[:show, :edit, :update, :destroy, :upgrade_priority, :downgrade_priority]
+  
   def index
+    @lists = @board.list
   end
 
   def show
   end
 
   def new
+    @list = List.new
+    render partial: "form"
+  end
+
+  def create
+    priority_when_added(@board)
+
+    #@list = @board.list.new(list_params)
+
+    if @list.save
+      redirect to board_list_path(@board, @list)
+    else
+      render :new
+    end
   end
 
   def edit
+    render partial: "form"
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to board_list_path(@board, @list)
+    else
+      render :edit
+    end
+  end
+
+  # when destroyed, check to see if the priority of the list is greater than the one deleted.
+  # if yes, loop through and -1 from the priority of all items there after.
+  def destroy
+    @list.destroy
+    @board.lists.each do |list|
+      if list.priority > destroyed.priority
+        list.priority = list.priority - 1
+      else 
+        redirect to board_list_path(@board, @list)
+      end
+      # if nothing needs to happen stay on the page?
+    redirect_to board_path(@board)
+  end
+
+  def upgrade_priority
+    list_down_priority = @board.lists.find_by(priority: @list.priority - 1)
+    @list.update(priority: list_down_priority.priority)
+    list_down_priority.update(priority: list_down_priority + 1)
+  end
+
+  def downgrade_priority
+    list_down_priority = @board.lists.find_by(priority: @list.priority - 1)
+    @list.update(priority: list_down_priority.priority)
+    list_down_priority.update(priority: list_down_priority + 1)
   end
 
   private
+  def set_board
+    @board = Board.fin(params[:board_id])
+  end
 
   def set_list
-    @list = List.find(params[:id])
+    @list = List.find(param[:id])
   end
+
   def list_params
-    params.require(:list).permit!
+    params.require(:list).(:name)
   end
 end
