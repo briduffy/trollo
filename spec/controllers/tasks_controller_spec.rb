2c378850b34701a983cb6fa83d18cd3e138d4c12
Dirk Dobson <dirkvancouver@live.com>
2018-09-11 16:06:27 -0600

    test for tasks controller done

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -14,15 +14,4 @@ RSpec.describe TasksController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET #create" do
-    it "returns http success" do
-      board = Board.create! valid_attributes
-      list = board.lists.create! valid_attributes
-      task = list.tasks.create! valid_attributes
-      get :create, params: {list_id: list.id}
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
