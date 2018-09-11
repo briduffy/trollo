eefe5d20e047926a80ddaee9c9f80768f664f709
Dirk Dobson <dirkvancouver@live.com>
2018-09-11 16:23:57 -0600

    board controller finish

--- spec/controllers/boards_controller_spec.rb
+++ spec/controllers/boards_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe BoardsController, type: :controller do
 
+  let(:valid_attributes) {
+    {name: 'almost there'}
+  }
+
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -11,7 +15,8 @@ RSpec.describe BoardsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      board = Board.create! valid_attributes
+      get :show, params: {id: board.id}
       expect(response).to have_http_status(:success)
     end
   end
@@ -25,7 +30,8 @@ RSpec.describe BoardsController, type: :controller do
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit
+      board = Board.create! valid_attributes
+      get :edit, params: {id: board.id}
       expect(response).to have_http_status(:success)
     end
   end
