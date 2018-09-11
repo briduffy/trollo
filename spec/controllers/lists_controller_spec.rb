badeb24a0c2ee40fe058f700fc41bfbe31c2bcf4
Dirk Dobson <dirkvancouver@live.com>
2018-09-11 16:20:17 -0600

    lists controller finish

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -2,30 +2,40 @@ require 'rails_helper'
 
 RSpec.describe ListsController, type: :controller do
 
+  let(:valid_attributes) {
+    { name: 'whatever'}
+  }
+
   describe "GET #index" do
     it "returns http success" do
-      get :index
+      board = Board.create! valid_attributes
+      get :index, params: {board_id: board.id}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      board = Board.create! valid_attributes
+      list = board.lists.create! valid_attributes
+      get :show, params: {board_id: board.id, id: list.id}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #new" do
     it "returns http success" do
-      get :new
+      board = Board.create! valid_attributes
+      get :new, params: {board_id: board.id}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit
+      board = Board.create! valid_attributes
+      list = board.lists.create! valid_attributes
+      get :edit, params: {board_id: board.id, id: list.id}
       expect(response).to have_http_status(:success)
     end
   end
